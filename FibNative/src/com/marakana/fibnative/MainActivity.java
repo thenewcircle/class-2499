50101d5069d731b9240102dfd9656a668cb250cc
Marko Gargenta <marko@marakana.com>
2012-11-27 08:14:24 -0800

    Implemented simple UI

--- FibNative/src/com/marakana/fibnative/MainActivity.java
+++ FibNative/src/com/marakana/fibnative/MainActivity.java
@@ -1,22 +1,28 @@
 package com.marakana.fibnative;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	EditText input;
+	TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        input = (EditText) findViewById(R.id.input);
+        output = (TextView) findViewById(R.id.output);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public void onClick(View v) {
+    		long n = Long.parseLong( input.getText().toString() );
+    		
+    		long resultJ = FibLib.fibJ(n);
+    		
+    		output.append( String.format("\n fibJ(%d) = %d", n, resultJ) );
     }
-    
 }
